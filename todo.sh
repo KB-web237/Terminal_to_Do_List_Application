b1e3c18d98b8b969f0a5f1e153baf5e8b6a8490e
KBrand <kbrandn25@gmail.com>
2025-11-22 11:55:32 +0100

    well modified code with additional functionality

--- todo.sh
+++ todo.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
-if [ "$1" == "add" ]; then
+if [ $# -eq 2 ]; then 
+   if [ "$1" == "add" ]; then
      if [ -f tasks.txt ]; then
-        x=$(wc -l < tasks.txt)
-    else
-        x=0
-    fi
-    y=$((x + 1))
-    echo "$y. $2" >> tasks.txt
-    echo "Task added:$y. $2"
-    elif [ "$1" == "list" ]; then
-    cat tasks.txt
-    elif [ "$1" = "del" ]; then
-    task_number=$2
-
-    if [ ! -f tasks.txt ]; then
-        echo "No tasks found."
+        num_line=$(wc -l < tasks.txt)
+       else
+        num_line=0
+     fi
+      line_num=$((num_line + 1))
+      echo ". $2" >> tasks.txt
+      echo "Task added:$line_num. $2"
+     elif [ "$1" == "del" ]; then
+      del_number=$2
+     if [ $del_number -gt $(wc -l < tasks.txt) ]; then
+        echo "Error: The number doesn't exist"
         exit 1
-    fi
-    # Delete the line that starts with: "number. "
-    sed -i "${task_number}d" tasks.txt
-    echo "Deleted task number $task_number"
+        else
+       sed -i "${del_number}d" tasks.txt
+      echo "Deleted task number $del_number"
+     fi
+    fi 
+    elif [ "$1" == "list" ]; then
+    cat -n tasks.txt
     else
-    echo "Usage: $0 {add|list|del} [task]"
+    echo "Usage: '$0 add task' or '$0 list' or '$0 del number'"
 fi
